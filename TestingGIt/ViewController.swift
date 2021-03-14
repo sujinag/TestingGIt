9ac92f6295286d71b4cc5d8a7a42549ed350c67e
sujeet <nag.sujeet@gmail.com>
2021-03-14 12:00:20 +0530

    sujeetChangedBcolor

--- TestingGIt/ViewController.swift
+++ TestingGIt/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .green
         // Do any additional setup after loading the view.
     }
 
